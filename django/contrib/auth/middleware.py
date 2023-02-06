5502d0efb805ee6018450e026c0c188c77b2ff57
Jon Janzen <jon@jonjanzen.com>
2023-02-05 20:07:32 -0500

    middleware

--- django/contrib/auth/middleware.py
+++ django/contrib/auth/middleware.py
@@ -93,6 +93,48 @@ class RemoteUserMiddleware(MiddlewareMixin):
             request.user = user
             auth.login(request, user)
 
+    async def aprocess_request(self, request):
+        # AuthenticationMiddleware is required so that request.user exists.
+        if not hasattr(request, "user"):
+            raise ImproperlyConfigured(
+                "The Django remote user auth middleware requires the"
+                " authentication middleware to be installed.  Edit your"
+                " MIDDLEWARE setting to insert"
+                " 'django.contrib.auth.middleware.AuthenticationMiddleware'"
+                " before the RemoteUserMiddleware class."
+            )
+        try:
+            username = request.META[self.header]
+        except KeyError:
+            # If specified header doesn't exist then remove any existing
+            # authenticated remote-user, or return (leaving request.user set to
+            # AnonymousUser by the AuthenticationMiddleware).
+            if self.force_logout_if_no_header:
+                user = await request.auser()
+                if user.is_authenticated:
+                    await self._aremove_invalid_user(request)
+            return
+        user = await request.auser()
+        # If the user is already authenticated and that user is the user we are
+        # getting passed in the headers, then the correct user is already
+        # persisted in the session and we don't need to continue.
+        if user.is_authenticated:
+            if user.get_username() == self.clean_username(username, request):
+                return
+            else:
+                # An authenticated user is associated with the request, but
+                # it does not match the authorized user in the header.
+                await self._aremove_invalid_user(request)
+
+        # We are seeing this user for the first time in this session, attempt
+        # to authenticate the user.
+        user = await auth.aauthenticate(request, remote_user=username)
+        if user:
+            # User is valid.  Set request.user and persist user in the session
+            # by logging the user in.
+            request.user = user
+            await auth.alogin(request, user)
+
     def clean_username(self, username, request):
         """
         Allow the backend to clean the username, if the backend defines a
@@ -121,6 +163,22 @@ class RemoteUserMiddleware(MiddlewareMixin):
         else:
             if isinstance(stored_backend, RemoteUserBackend):
                 auth.logout(request)
+
+    async def _aremove_invalid_user(self, request):
+        """
+        Remove the current authenticated user in the request which is invalid
+        but only if the user is authenticated via the RemoteUserBackend.
+        """
+        try:
+            stored_backend = load_backend(
+                request.session.get(auth.BACKEND_SESSION_KEY, "")
+            )
+        except ImportError:
+            # backend failed to load
+            auth.alogout(request)
+        else:
+            if isinstance(stored_backend, RemoteUserBackend):
+                auth.alogout(request)
 
 
 class PersistentRemoteUserMiddleware(RemoteUserMiddleware):
